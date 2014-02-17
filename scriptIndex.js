d44ed8bc82377a1ecaa8df49e5bcc1da2649137d
MarcosVn <marcos.vnaraujo@gmail.com>
2014-02-16 23:03:39 -0300

    update css/table

--- scriptIndex.js
+++ scriptIndex.js
@@ -3,3 +3,16 @@ function index_ctr($scope){
 	$scope.open_searchs = false;
 	$scope.teste = [1,2,3,4,5,6,7,8,9,10,11]
 }
+
+function index_ctr($scope){
+	$scope.active_menu = [false,false];
+	$scope.open_searchs = false;
+	$scope.produtos = [{'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},]
+}
