ba1c440d7d19e94efb3df1a3a78552c933b4ab3a
Bustamante <marcos.bustamante@fatec.sp.gov.br>
2014-02-19 17:53:35 -0300

    apresentacao de lista

--- scriptIndex.js
+++ scriptIndex.js
@@ -1,12 +1,18 @@
 function index_ctr($scope){
 	$scope.active_menu = [false,false];
 	$scope.open_searchs = false;
-	$scope.produtos = [{'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
-					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
-					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
-					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
-					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
-					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
-					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},
-					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00'},]
+	$scope.product_view_selected = 'list';
+	$scope.produtos = [{'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},
+					   {'name':'X-box One','img':'imgsTabela/xbox1.jpg','distance':'500m','price':'R$ 2.230,00','shop':'Loja do Jão','street':'Rua 1 - 158, Centro'},]
+
+	$scope.change_product_view = function(element){
+		$scope.product_view_selected = element;
+	}
 }
